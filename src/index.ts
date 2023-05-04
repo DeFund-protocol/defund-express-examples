92842a6e71da4a4fef6743ae919c4884a0e3537a
EtherPilled <richard.clare@upr.edu>
2023-05-04 14:51:03 -0400

    End points and Request Response
    In this commit I added the trade endpoint that allows GET() requests to that endpoint with easier specification of the parameters for the trade_defund function and sdk usage. By allowing these type of calls we open the doors for multiple programming language to be able to operate with DeFund SDK through this new API.

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,34 @@
 // Import the express in typescript file
 import express from 'express';
 import { trade_defund } from './defund';
+import { Request, Response } from "express";
 
 // Initialize the express engine
 const app: express.Application = express();
 
 // Take a port 3000 for running server.
 const port: number = 3000;
-
+app.use(express.urlencoded({ extended: true }))
 // Handling '/' Request
-app.get('/', async (_req, _res) => {
-	const fundAddress = '';
-	const fromToken = '';
-	const toToken = '';
-	const slippage = 1; // 1%
-	const share = 10; // 10%
-	const network = '';
-	const options = {
-		gasLimit: 1000000,
-		// gasPrice: 60, // 60 gwei
-	} // optional
-	const tx = await trade_defund(fundAddress, fromToken, toToken, slippage, share, network, options);
-	_res.send(tx);
+app.get("/trade", async (req: Request, res: Response) => {
+  //try {
+    let protocol: string = 'defund';
+    if (req.query.protocol) protocol = req.query.protocol as string;
+    const assetA = req.query.from as string;
+    const assetB = req.query.to as string;
+    const share = req.query.share as string;
+    const amount = req.query.amount as string;
+    const slippage = req.query.slippage as string;
+    const poolAddress = req.query.pool as string;
+    const network = req.query.network as string
+    const options = {
+            gasLimit: 1000000,
+            gasPrice: 60, // 60 gwei
+    }
+    const tx = await trade_defund(poolAddress, assetA, assetB, Number(slippage), Number(share), network, options);
 });
-
 // Server setup
 app.listen(port, () => {
-	console.log(`TypeScript with Express
-		http://localhost:${port}/`);
+        console.log(`DeFund API TypeScript with Express
+                http://localhost:${port}/`);
 });
-
